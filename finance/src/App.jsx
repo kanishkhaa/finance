d466b0b9e09664cb1f355c119e2f1e1bb7809760
kanishkhaa <kanishkhaams@gmail.com>
2025-12-10 12:56:41 +0530

    initial setup

---
+++ finance/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+export default function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-gray-800">
+        Hello World
+      </h1>
+    </div>
+  );
+}
